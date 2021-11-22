0a2098770740511ce2726334ebe70b4a7aa6235d
José Gabriel Almendares <gabrielalmendares24@gmail.com>
2021-11-22 11:36:10 -0600

    Update Inventario.js

--- src/pages/Inventario.js
+++ src/pages/Inventario.js
@@ -13,6 +13,7 @@ import {
 import { url } from '../utilities/utilities.js'
 import escaner from '../assets/images/escaner.png'
 import editar from '../assets/images/editar_producto.png'
+import control_stock from '../assets/images/control_stock.jpg'
 
 class Inventario extends Component {
   constructor(props) {
@@ -20,7 +21,8 @@ class Inventario extends Component {
     this.state = {
       url: url(),
       matricula: false,
-      editar_producto: false
+      editar_producto: false,
+      control_stock: false
     }
   }
 
@@ -30,7 +32,13 @@ class Inventario extends Component {
         this.setState({ matricula: true });
         break;
       case 'editar_productos':
-        this.setState({ editar_producto: true })
+        this.setState({ editar_producto: true });
+        break;
+      case 'control_stock':
+        this.setState({ control_stock: true });
+        break;
+      default:
+        break;
     }
   }
 
@@ -42,6 +50,10 @@ class Inventario extends Component {
     
     if(this.state.editar_producto){
       return (<Redirect to={'/editar-info-productos'} />)
+    }
+
+    if(this.state.control_stock){
+      return (<Redirect to={'/control-stock'} />)
     }
 
     return (
@@ -65,6 +77,18 @@ class Inventario extends Component {
                   height: "140px", borderColor: "#C0C0C0",
                   borderWidth: "1px", borderStyle: "solid"
                 }}><IonImg src={editar} style={{ height: "100%" }}></IonImg></IonCol>
+              </IonRow>
+              
+              <IonRow>
+                <IonCol size="6" onClick={() => this.redirigir('control_stock')} style={{
+                  height: "140px", borderColor: "#C0C0C0",
+                  borderWidth: "1px", borderStyle: "solid", backgroundSize: "cover"
+                }}><IonImg src={control_stock} style={{ height: "100%" }}></IonImg></IonCol>
+
+                <IonCol size="6" onClick={() => this.redirigir('editar_productos')} style={{
+                  height: "140px", borderColor: "#C0C0C0",
+                  borderWidth: "1px", borderStyle: "solid"
+                }}></IonCol>
               </IonRow>
           </IonGrid>
         </IonContent>
