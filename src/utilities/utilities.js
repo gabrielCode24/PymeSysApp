cb7f247391971ea9d958a29c717f6fd3c5ac18fd
José Gabriel Almendares <gabrielalmendares24@gmail.com>
2021-11-22 11:37:04 -0600

    Update utilities.js

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -27,7 +27,7 @@ export function prepararPost(values, set, action = "setJsons", structure = "json
       headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
       body: formBody
     };
-    console.log(requestOptions);
+    
     return requestOptions;
 
   } else if (structure == "jsonArray") {
